04ff16a131426d3d5834b62a70a6928e4b572bf0
Lai <fairytail871202@gmail.com>
2022-05-13 02:55:21 +0800

    'check'

--- Binchilling.js
+++ Binchilling.js
@@ -64,10 +64,9 @@ function check_email_verification_code(){
     setTimeout(function(){
       document.getElementById('verify_email_page').style.display='none';
       document.getElementById('verify_success_img').style.display='none';
+      document.getElementById('add_new_account_page').style.display='block';
+      document.getElementById('wallet').style.display='block';
       document.getElementById('wallet_navbar').style.display='block';
-      document.getElementById('wallet_overview').style.display='block';
-      document.getElementById('privacybar').style.display='block';
-      document.getElementById('bottom_area').style.display='block';
     }, 2000);
   }
   else{
@@ -92,9 +91,17 @@ function turn_to_wallet_page(){
   document.getElementById('wallet_overview').style.display='block';
   document.getElementById('privacybar').style.display='block';
   document.getElementById('bottom_area').style.display='block';
+  document.getElementById("Risk").style.visibility = 'hidden';
+  d3.select("svg").remove();
+  draw();  // Overview chart
+  show_risk_index('overview');
 }
 
 function turn_to_specific_wallet(btn_id){
+  document.getElementById("Risk_NFT").style.visibility = 'hidden';
+  d3.select("svg").remove();
+  draw_NFT_chart();
+  show_risk_index('NFT');
   for(var wallet in wallet_pages){
     if(document.getElementById(btn_id).textContent == wallet_pages[wallet]) {
       var next_wallet_page = wallet_pages[wallet];
@@ -102,13 +109,17 @@ function turn_to_specific_wallet(btn_id){
     }   
   }
   document.getElementById('wallet_overview').style.display='none';
-  var next_wallet_page_id = 'wallet_'+next_wallet_page;
+  var next_wallet_page_id = 'wallet_'+ next_wallet_page;
   document.getElementById(next_wallet_page_id).style.display='block';
 }
 
 function turn_to_overview_page(){
   document.getElementById('wallet_NFT').style.display='none';
   document.getElementById('wallet_overview').style.display='block';
+  document.getElementById("Risk").style.visibility = 'hidden';
+  d3.select("svg").remove();
+  draw();
+  show_risk_index('overview');
 }
 
 function turn_to_NFT_Martket(){
@@ -399,6 +410,9 @@ function close_wallet_whole_asset_currency_menu(currency_id){
 
 
 $(document).ready(function(){
+
+  $('[data-toggle="tooltip"]').tooltip();
+
   $("#wallet_account_choice").click(function(){
     console.log("wallet");
     if($("#category_btw_account").offset().left>='65%'){
@@ -424,3 +438,261 @@ function homepage_turn_to_signup_page(btn_id){
 }
 
 
+
+
+
+
+/* ****** ------------------------------------------------------- Chart Animation--------------------------------------------------------- ****** */
+
+let data = [296210.326, 175832.331, 91722.462, 30017.135, 25127.746]; // ["296210.326", "175832.331", "91722.462", "30017.135", "25127.746"];
+let colors = ["#B8CAD6", "#E9E9EB", "#528CA2", "#42506B", "#FF7582"];
+let NFT_data = [1651.428965, 1455.681035];
+let NFT_colors = ["#E9E9EB", "#FF7582"];
+
+let sizes = {
+  innerRadius: 0,
+  outerRadius: 250
+};
+
+let durations = {
+  entryAnimation: 3000
+};
+
+function draw() {
+  // d3.select("#chart").html("");
+  const Name = ["NFT", "Stock" , "Crypto" ,"CD (For)", "CD (NTD)"]  
+  let generator = d3.pie()
+    .sort(null);
+
+  let chart = generator(data);
+  let arcs = d3.select("#chart")
+    .append('svg')
+    .attr('width', 650)
+    .attr('height', 650)
+    .append("g")
+    .attr("id", "piechart")
+    .attr("transform", "translate(400, 250)");
+  
+  var path = arcs.selectAll('path')
+    .data(chart)
+    .enter()
+    .append('path')
+    .style("fill", (d, i) => colors[i]);
+
+
+  /* ------------------ Tooltip ----------------- */
+  let tooltips = d3.select("#chart")
+      .append("div")
+      .attr('class', 'tooltip')
+      .style("opacity", 0)   
+      .style("position", "absolute")
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
+
+  tooltips.append('div')
+    .attr('class','count');
+
+  tooltips.append('div')
+    .attr('class','percent');
+
+  // 加上滑鼠事件
+  d3.select('#piechart')
+    .style('cursor', 'pointer')
+    .on('mouseover', function() { 
+      tooltips.style("opacity", 1) 
+    })
+    .on("mousemove", function(event, d) {
+      var percent = ["296210.326", "175832.331", "91722.462", "30017.135", "25127.746"]
+      let riskNum = [65, 54, 49, 45, 46];
+      var total = d3.sum(percent);
+      function myFunction(item, index, arr) {
+        arr[index] = Math.round(1000 * item / total)/10;
+      }          
+      percent.forEach(myFunction);
+
+      // console.log("percent: " + percent[0]);
+        
+      let pt = d3.pointer(event) // 抓圓點位置
+      let i;
+
+      if(pt[0] > 30){
+        i = 0;
+      }else if(pt[0] > -63 && pt[1] < 0){
+        i = 4;
+      }else if(pt[0] < 30 && pt[1] > -17){
+        i = 1;
+      }else if(pt[0] < 0 &&  -209 < pt[1] && pt[1] < -17){
+        i = 2;
+      }else {
+        i = 3;
+      }
+
+      tooltips.style("opacity", 1)
+        .style('left', (2* pt[0] + 450) +'px') // 設定tooltips位置
+        .style('top', (2*pt[1] + 450) +'px')
+        .html(" " + Name[i] + "; RI: " + riskNum[i] + "<br>(" + percent[i] + "%)") // 抓到綁定在DOM元素的資料  
+        .style("font-family", "SFProDisplay-Heavy")
+        .style("font-size", "20px")
+      })
+    .on('mouseleave', function(){ //設定滑鼠離開時tooltips隱藏
+      tooltips.style("opacity", 0)
+    });
+
+  /* ------------------ Sth ------------------ */
+  let angleInterpolation = d3.interpolate(generator.startAngle()(), generator.endAngle()());
+  let innerRadiusInterpolation = d3.interpolate(0, sizes.innerRadius);
+  let outerRadiusInterpolation = d3.interpolate(0, sizes.outerRadius);
+
+  let arc = d3.arc();
+
+  /* ------------------ Animation ------------------ */
+  path.transition()
+    .duration(durations.entryAnimation)
+    .attrTween("d", d => {
+      let originalEnd = d.endAngle;
+      return t => {
+        let currentAngle = angleInterpolation(t);
+        if (currentAngle < d.startAngle) {
+          return "";
+        }
+
+        d.endAngle = Math.min(currentAngle, originalEnd);
+
+        return arc(d);
+      };
+    });
+
+  d3.select("#chart")
+    .transition()
+    .duration(durations.entryAnimation)
+    .tween("arcRadii", () => {
+      return t => arc
+        .innerRadius(innerRadiusInterpolation(t))
+        .outerRadius(outerRadiusInterpolation(t));
+    });
+}
+
+function show_risk_index(page){
+  setTimeout(function () {
+    switch(page){
+      case "overview":
+        document.getElementById("Risk").style.visibility = "visible";
+        break;
+      case "NFT":
+        document.getElementById("Risk_NFT").style.visibility = "visible";
+        break;
+    }
+  }, 2500);
+}
+
+
+function draw_NFT_chart() {
+  const Name = ["Metamask", "Wallet Connect"]  
+  let generator = d3.pie()
+    .sort(null);
+
+  let chart = generator(NFT_data);
+  let arcs = d3.select("#chart_NFT")
+    .append('svg')
+    .attr('width', 650)
+    .attr('height', 650)
+    .append("g")
+    .attr("id", "piechart_NFT")
+    .attr("transform", "translate(400, 250)");
+  
+  var path = arcs.selectAll('path')
+    .data(chart)
+    .enter()
+    .append('path')
+    .style("fill", (d, i) => NFT_colors[i]);
+
+
+  /* ------------------ Tooltip ----------------- */
+  let tooltips = d3.select("#chart_NFT")
+      .append("div")
+      .attr('class', 'tooltip')
+      .style("opacity", 0)   
+      .style("position", "absolute")
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
+
+  tooltips.append('div')
+    .attr('class','count');
+
+  tooltips.append('div')
+    .attr('class','percent');
+
+  // 加上滑鼠事件
+  d3.select('#piechart_NFT')
+    .style('cursor', 'pointer')
+    .on('mouseover', function() { 
+      tooltips.style("opacity", 1) 
+    })
+    .on("mousemove", function(event, d) {
+      var percent = [ "1651.428965", "1455.681035"];
+      var total = d3.sum(percent);
+      function myFunction(item, index, arr) {
+        arr[index] = Math.round(1000 * item / total)/10;
+      }          
+      percent.forEach(myFunction);
+        
+      let pt = d3.pointer(event) // 抓圓點位置
+
+      let i;
+
+      if(pt[0] > 0 || (-35 < pt[0] && pt[0] < 0 && pt[1] > 0)){
+        i = 0;
+      }else if(pt[0] < 0){
+        i = 1;
+      }
+
+      tooltips.style("opacity", 1)
+        .style('left', (1.8* pt[0] + 450) +'px') // 設定tooltips位置
+        .style('top', (1.8*pt[1] + 450) +'px')
+        .html(" " + Name[i] + " <br> (" + percent[i] + "%)") // 抓到綁定在DOM元素的資料  
+        .style("font-family", "SFProDisplay-Heavy")
+        .style("font-size", "20px")
+      })
+    .on('mouseleave', function(){ //設定滑鼠離開時tooltips隱藏
+      tooltips.style("opacity", 0)
+    });
+
+  /* ------------------ Sth ------------------ */
+  let angleInterpolation = d3.interpolate(generator.startAngle()(), generator.endAngle()());
+  let innerRadiusInterpolation = d3.interpolate(0, sizes.innerRadius);
+  let outerRadiusInterpolation = d3.interpolate(0, sizes.outerRadius);
+
+  let arc = d3.arc();
+
+  /* ------------------ Animation ------------------ */
+  path.transition()
+    .duration(durations.entryAnimation)
+    .attrTween("d", d => {
+      let originalEnd = d.endAngle;
+      return t => {
+        let currentAngle = angleInterpolation(t);
+        if (currentAngle < d.startAngle) {
+          return "";
+        }
+        d.endAngle = Math.min(currentAngle, originalEnd);
+        return arc(d);
+      };
+    });
+
+  d3.select("#chart_NFT")
+    .transition()
+    .duration(durations.entryAnimation)
+    .tween("arcRadii", () => {
+      return t => arc
+        .innerRadius(innerRadiusInterpolation(t))
+        .outerRadius(outerRadiusInterpolation(t));
+    });
+}
--- chart.js
+++ chart.js
@@ -1,48 +1,250 @@
-const width = 514, height = 514;
-const radius = width / 2 ;
+$(document).ready(function () {
+  draw();
+  draw_NFT_chart();
+  $('[data-toggle="tooltip"]').tooltip();
+  setTimeout(function () {
+    document.getElementById("Risk").style.visibility = "visible";
+    document.getElementById("Risk_NFT").style.visibility = "visible";
+  }, 2500);
+});
 
-// Input data
-const data = {"NFT": "296210.326", "Stock": "175832.331", "Crypto": "91722.462", "CD (For)": "30017.135", "CD (NTD)": "25127.746"}
-const color = d3.scaleOrdinal()
-  .range(["#9BB3C6", "#E9E9EB", "#528CA2", "#355B7F","#FF7582"])
+
+let data = [296210.326, 175832.331, 91722.462, 30017.135, 25127.746]; // ["296210.326", "175832.331", "91722.462", "30017.135", "25127.746"];
+let colors = ["#B8CAD6", "#E9E9EB", "#528CA2", "#42506B", "#FF7582"];
+let NFT_data = [1651.428965, 1455.681035];
+let NFT_colors = ["#E9E9EB", "#FF7582"];
+
+let sizes = {
+  innerRadius: 0,
+  outerRadius: 250
+};
+
+let durations = {
+  entryAnimation: 3000
+};
+
+function draw() {
+  // d3.select("#chart").html("");
+  const Name = ["NFT", "Stock" , "Crypto" ,"CD (For)", "CD (NTD)"]  
+  let generator = d3.pie()
+    .sort(null);
+
+  let chart = generator(data);
+  let arcs = d3.select("#chart")
+    .append('svg')
+    .attr('width', 650)
+    .attr('height', 650)
+    .append("g")
+    .attr("id", "piechart")
+    .attr("transform", "translate(400, 250)");
+  
+  var path = arcs.selectAll('path')
+    .data(chart)
+    .enter()
+    .append('path')
+    .style("fill", (d, i) => colors[i]);
 
 
-// append the svg object to the div called 'dataviz'
-const svg = d3.select("#dataviz")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", `translate(${width/2}, ${height/2})`)
-    .attr("id", "Tooltip");
+  /* ------------------ Tooltip ----------------- */
+  let tooltips = d3.select("#chart")
+      .append("div")
+      .attr('class', 'tooltip')
+      .style("opacity", 0)   
+      .style("position", "absolute")
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
 
-// Compute the position
-const pie = d3.pie()
-  .value(function(d) {return d[1]})
+  tooltips.append('div')
+    .attr('class','count');
 
-const data_ready = pie(Object.entries(data))
+  tooltips.append('div')
+    .attr('class','percent');
 
-let labelArc = d3.arc()
-.outerRadius(150)
-.innerRadius(radius)
+  // 加上滑鼠事件
+  d3.select('#piechart')
+    .style('cursor', 'pointer')
+    .on('mouseover', function() { 
+      tooltips.style("opacity", 1) 
+    })
+    .on("mousemove", function(event, d) {
+      var percent = ["296210.326", "175832.331", "91722.462", "30017.135", "25127.746"]
+      let riskNum = [65, 54, 49, 45, 46];
+      var total = d3.sum(percent);
+      function myFunction(item, index, arr) {
+        arr[index] = Math.round(1000 * item / total)/10;
+      }          
+      percent.forEach(myFunction);
 
-// Build the pie chart
-svg
-  .selectAll('whatever')
-  .data(data_ready)
-  .join('path')
-  .attr('d', d3.arc()
-    .innerRadius(0)
-    .outerRadius(radius)  
-  )
-  .attr('fill', function(d){ return(color(d.data[1])) })
+      // console.log("percent: " + percent[0]);
+        
+      let pt = d3.pointer(event) // 抓圓點位置
+      let i;
 
-svg
-  .selectAll('whatever')
-  .data(data_ready)
-  .join('text')
-  .text(function(d){ return d.data[0]})
-  .attr("transform", function(d) { return `translate(${ labelArc.centroid(d)})`})
-  .style("text-anchor", "middle")
-  .style("font", "15px SFPRODISPLAYREGULAR")
-  .style("font-size", 20)
+      if(pt[0] > 30){
+        i = 0;
+      }else if(pt[0] > -63 && pt[1] < 0){
+        i = 4;
+      }else if(pt[0] < 30 && pt[1] > -17){
+        i = 1;
+      }else if(pt[0] < 0 &&  -209 < pt[1] && pt[1] < -17){
+        i = 2;
+      }else {
+        i = 3;
+      }
+
+      tooltips.style("opacity", 1)
+        .style('left', (2* pt[0] + 450) +'px') // 設定tooltips位置
+        .style('top', (2*pt[1] + 450) +'px')
+        .html(" " + Name[i] + "; RI: " + riskNum[i] + "<br>(" + percent[i] + "%)") // 抓到綁定在DOM元素的資料  
+        .style("font-family", "SFProDisplay-Heavy")
+        .style("font-size", "20px")
+      })
+    .on('mouseleave', function(){ //設定滑鼠離開時tooltips隱藏
+      tooltips.style("opacity", 0)
+    });
+
+  /* ------------------ Sth ------------------ */
+  let angleInterpolation = d3.interpolate(generator.startAngle()(), generator.endAngle()());
+  let innerRadiusInterpolation = d3.interpolate(0, sizes.innerRadius);
+  let outerRadiusInterpolation = d3.interpolate(0, sizes.outerRadius);
+
+  let arc = d3.arc();
+
+  /* ------------------ Animation ------------------ */
+  path.transition()
+    .duration(durations.entryAnimation)
+    .attrTween("d", d => {
+      let originalEnd = d.endAngle;
+      return t => {
+        let currentAngle = angleInterpolation(t);
+        if (currentAngle < d.startAngle) {
+          return "";
+        }
+
+        d.endAngle = Math.min(currentAngle, originalEnd);
+
+        return arc(d);
+      };
+    });
+
+  d3.select("#chart")
+    .transition()
+    .duration(durations.entryAnimation)
+    .tween("arcRadii", () => {
+      return t => arc
+        .innerRadius(innerRadiusInterpolation(t))
+        .outerRadius(outerRadiusInterpolation(t));
+    });
+}
+
+
+function draw_NFT_chart() {
+  const Name = ["Metamask", "Wallet Connect"]  
+  let generator = d3.pie()
+    .sort(null);
+
+  let chart = generator(NFT_data);
+  let arcs = d3.select("#chart_NFT")
+    .append('svg')
+    .attr('width', 650)
+    .attr('height', 650)
+    .append("g")
+    .attr("id", "piechart_NFT")
+    .attr("transform", "translate(400, 250)");
+  
+  var path = arcs.selectAll('path')
+    .data(chart)
+    .enter()
+    .append('path')
+    .style("fill", (d, i) => NFT_colors[i]);
+
+
+  /* ------------------ Tooltip ----------------- */
+  let tooltips = d3.select("#chart_NFT")
+      .append("div")
+      .attr('class', 'tooltip')
+      .style("opacity", 0)   
+      .style("position", "absolute")
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "2px")
+      .style("border-radius", "5px")
+      .style("padding", "5px");
+
+  tooltips.append('div')
+    .attr('class','count');
+
+  tooltips.append('div')
+    .attr('class','percent');
+
+  // 加上滑鼠事件
+  d3.select('#piechart_NFT')
+    .style('cursor', 'pointer')
+    .on('mouseover', function() { 
+      tooltips.style("opacity", 1) 
+    })
+    .on("mousemove", function(event, d) {
+      var percent = [ "1651.428965", "1455.681035"];
+      var total = d3.sum(percent);
+      function myFunction(item, index, arr) {
+        arr[index] = Math.round(1000 * item / total)/10;
+      }          
+      percent.forEach(myFunction);
+        
+      let pt = d3.pointer(event) // 抓圓點位置
+
+      let i;
+
+      if(pt[0] > 0 || (-35 < pt[0] && pt[0] < 0 && pt[1] > 0)){
+        i = 0;
+      }else if(pt[0] < 0){
+        i = 1;
+      }
+
+      tooltips.style("opacity", 1)
+        .style('left', (1.8* pt[0] + 450) +'px') // 設定tooltips位置
+        .style('top', (1.8*pt[1] + 450) +'px')
+        .html(" " + Name[i] + " <br> (" + percent[i] + "%)") // 抓到綁定在DOM元素的資料  
+        .style("font-family", "SFProDisplay-Heavy")
+        .style("font-size", "20px")
+      })
+    .on('mouseleave', function(){ //設定滑鼠離開時tooltips隱藏
+      tooltips.style("opacity", 0)
+    });
+
+  /* ------------------ Sth ------------------ */
+  let angleInterpolation = d3.interpolate(generator.startAngle()(), generator.endAngle()());
+  let innerRadiusInterpolation = d3.interpolate(0, sizes.innerRadius);
+  let outerRadiusInterpolation = d3.interpolate(0, sizes.outerRadius);
+
+  let arc = d3.arc();
+
+  /* ------------------ Animation ------------------ */
+  path.transition()
+    .duration(durations.entryAnimation)
+    .attrTween("d", d => {
+      let originalEnd = d.endAngle;
+      return t => {
+        let currentAngle = angleInterpolation(t);
+        if (currentAngle < d.startAngle) {
+          return "";
+        }
+        d.endAngle = Math.min(currentAngle, originalEnd);
+        return arc(d);
+      };
+    });
+
+  d3.select("#chart_NFT")
+    .transition()
+    .duration(durations.entryAnimation)
+    .tween("arcRadii", () => {
+      return t => arc
+        .innerRadius(innerRadiusInterpolation(t))
+        .outerRadius(outerRadiusInterpolation(t));
+    });
+}
